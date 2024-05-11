a7813ab0e1b81548aea37d1f68766dc94a887542
Ahmed Raza <araza2125012.pgc@gmail.com>
2024-05-11 15:13:02 +0500

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
@@ -7,7 +5,7 @@
 #include <fstream>
 #include "Person.h" // Assuming Person class is defined in Person.h
 
-class Employee : protected Person
+class Employee : public Person
 {
 private:
     string employee_position;
@@ -18,25 +16,19 @@ public:
     Employee();
     Employee(int person_id, string person_name, string employee_position, float employee_salary);
     Employee(const Employee& obj);
-
-    // Setters
     void set_employee_position(string employee_position);
     void set_employee_salary(float employee_salary);
-
-    // Getters
     string get_employee_position();
     float get_employee_salary();
-
-    // Utility functions
     string tostring();
-    void Employee_Menu();
-    //void store_to_file();
-    //void view_from_file();
     void promote(string new_position);
-
-    // Stream operators
+    virtual void store_to_file();
+    virtual void view_from_file();
+    void update();
+    void search();
+    void del();
+    void menu(Employee& obj);
     friend ostream& operator<<(ostream& cout, const Employee& obj);
     friend istream& operator>>(istream& cin, Employee& obj);
 };
-
 #endif // EMPLOYEE_H
