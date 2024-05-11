0b25d50b3d42cbaa6e65464fd80f38f812890735
Ahmed Raza <araza2125012.pgc@gmail.com>
2024-05-11 15:10:20 +0500

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,4 +1,3 @@
-#pragma once 
 #ifndef PERSON_H
 #define PERSON_H
 
@@ -11,31 +10,19 @@ class Person {
 private:
     int person_id;
     string person_name;
-
 public:
     Person();
-    Person(int person_id,string person_name);
+    Person(int person_id, string person_name);
     Person(const Person& obj);
-
     //setter methods
     void set_person_id(int person_id);
     void set_person_name(const string person_name);
-
     // Getter methods
     int get_person_id() const;
     string get_person_name() const;
-
     //Member functions
-    virtual string tostring() const = 0;
-    virtual void Customer_Menu() = 0;
-    virtual void Employee_Menu() = 0;
-    // void store_to_file();
-    //void view_from_file();
-
-    //Assignment operators
-    friend ostream& operator<<(ostream& cout, const Person& obj);
-    friend istream& operator>>(istream& cin, Person& obj);
-
+    string tostring() ;
+    virtual void view_from_file() = 0;
+    virtual void store_to_file() = 0;
 };
-
 #endif 
