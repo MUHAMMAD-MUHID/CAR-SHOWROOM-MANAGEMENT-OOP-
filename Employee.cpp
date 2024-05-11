17756a2a9754ff355fba3487f6ba18aefe74bbf9
Ahmed Raza <araza2125012.pgc@gmail.com>
2024-05-11 15:12:46 +0500

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -18,7 +18,7 @@ Employee::Employee(const Employee& obj) : Person(obj)
 {
     this->employee_position = obj.employee_position;
     this->employee_salary = obj.employee_salary;
-} 
+}
 
 void Employee::set_employee_position(string employee_position) {
     this->employee_position = employee_position;
@@ -36,180 +36,206 @@ float Employee::get_employee_salary() {
 
 // Member functions
 string Employee::tostring() {
-    return Person::tostring() + employee_position + to_string(employee_salary);
+    return Person::tostring() + "\t" + employee_position + "\t" + to_string(employee_salary) + "\n";
 }
-void Employee::Employee_Menu()
-{
-    int option, person_id, confirm;
-    string person_name, employee_position;
-    float employee_salary;
-
-    do
-    {
-        cout << "\nSelect Option : \n1.ADD\n2.VIEW\n3.SEARCH\n4.UPDATE\n5.DELETE";
-        cin >> option;
-        switch (option)
-        {
-        case 1:
-        {
-            ofstream file("Employee.txt", ios::app);
-            if (file.is_open())
-            {
-                // Prompt user for input
-                cout << "Enter Employee ID: ";
-                cin >> person_id;
-                cin.ignore(); // Ignore newline character in input buffer
-                cout << "Enter Employee name: ";
-                getline(cin, person_name);
-                cout << "Enter Emplyee position: ";
-                getline(cin, employee_position);
-                cout << "Enter Employee salary: ";
-                cin >> employee_salary;
-
-                // Write data to file
-                file << person_id << "\t" << person_name << "\t" << employee_position << "\t" << employee_salary << endl;
-
-                file.close();
-                cout << "Employee data stored successfully!" << endl;
-
-            }
-            else
-            {
-                cerr << "Unable to open file!" << endl;
-            }
-            break;
-        }
-        case 2:
-        {
-            ifstream fin("Employee.txt");
-            if (fin.is_open()) {
-                string line;
-                while (getline(fin, line)) {
-                    cout << line << endl;
-                }
-                fin.close();
-            }
-            else {
-                cerr << "Unable to open file!" << endl;
-            }
-            break;
-        }
-        case 3:
-        {
-            int temp;
-            bool flag = false;
-            ifstream fin("Employee.txt");
-            cout << "\nEnter ID of Employee : "; cin >> temp;
-            while (fin >> person_id >> person_name >> employee_position >> employee_salary)
-            {
-                if (temp == person_id)
-                {
-                    flag = true;
-                    cout << "\nEmployee ID : " << person_id << "\nEmployee NAME : " << person_name << "\nEmployee emplyee position : "
-                        << employee_position << "\nEmployee employee salary : " << employee_salary;
-                }
-
-            }
-            if (flag == false)
-            {
-                cout << "\nRecord not found.";
-            }
-            break;
-        }
-        case 4:
-        {
-            int temp;
-            bool flag = false;
-            ifstream fin_file("Employee.txt");
-            ofstream fout_file("temp.txt", ios::app);
-            cout << "\nEnter ID of Employee : "; cin >> temp;
-            while (fin_file >> person_id >> person_name >> employee_position >> employee_salary)
-            {
-                if (temp == person_id)
-                {
-                    flag = true;
-                    cout << "Enter Employee ID: ";
-                    cin >> person_id;
-                    cin.ignore(); // Ignore newline character in input buffer
-                    cout << "Enter Employee name: ";
-                    getline(cin, person_name);
-                    cout << "Enter Emplyee position: ";
-                    getline(cin, employee_position);
-                    cout << "Enter Employee salary: ";
-                    cin >> employee_salary;
-
-                    set_person_id(person_id);
-                    set_person_name(person_name);
-                    set_employee_position(employee_position);
-                    set_employee_salary(employee_salary);
-                }
-                fout_file << "\n" << person_id << "\t" << person_name << "\t" << employee_position << "\t" << employee_salary;
-            }
-            fin_file.close();
-            fout_file.close();
-            remove("Employee.txt");
-            rename("temp.txt", "Employee.txt");
-            if (flag == false)
-            {
-                cout << "\nRecord not found.";
-            }
-            break;
-        }
-        case 5:
-        {
-            int temp;
-            cout << "Enter the ID of the Showroom to delete: ";
-            cin >> temp;
-            ifstream fin("Employee.txt");
-            ofstream fout("temp.txt");
-            if (fin.is_open() && fout.is_open()) {
-                bool found = false;
-                while (fin >> person_id >> person_name >> employee_position >> employee_salary) {
-                    if (temp == person_id) {
-                        found = true;
-                    }
-                    else {
-                        fout << "\n" << person_id << "\t" << person_name << "\t" << employee_position << "\t" << employee_salary;
-                    }
-                }
-                fin.close();
-                fout.close();
-                remove("Employee.txt");
-                rename("temp.txt", "Employee.txt");
-                if (!found) {
-                    cout << "Showroom with ID " << temp << " not found." << endl;
-                }
-                else {
-                    cout << "Showroom with ID " << temp << " deleted successfully." << endl;
-                }
-            }
-            else {
-                cerr << "Error: Unable to open file for reading or writing." << endl;
-            }
-            break;
-        }
-        }
-        cout << "\nEnter 1 to use menu again.";
-        cin >> confirm;
-    } while (confirm == 1);
-
-}
-
 void Employee::promote(string new_position) {
     employee_position = new_position;
+}
+void Employee::store_to_file(){
+    ofstream fout("Employee.txt", ios::app);
+    if (fout.is_open()) {
+        fout << tostring() << endl;
+        fout.close();
+        cout << "\nData Stored To Employee  File";
+    }
+    else {
+        cerr << "Error: Unable to open file for writing." << endl;
+    }
+}
+void Employee::view_from_file(){
+    ifstream fin("Employee.txt");
+    if (fin.is_open()) {
+        string data;
+        while (getline(fin, data)) {
+            cout << data << endl;
+        }
+        fin.close();
+    }
+    else {
+        cerr << "Error: Unable to open file for reading." << endl;
+    }
+}
+void Employee::update() {
+    int id;
+    cout << "Enter the ID of the Employee to update: ";
+    cin >> id;
+    ifstream fin("Employee.txt");
+    ofstream fout("temp.txt");
+    if (fin.is_open() && fout.is_open()) {
+        bool found = false;
+        int option, Employee_id;
+        string Employee_name;
+        while (fin >> Employee_id >> Employee_name >>employee_position>>employee_salary) {
+            if (Employee_id == id) {
+                found = true;
+                cout << "Enter Employee ID: ";
+                cin >> Employee_id;
+                cin.ignore();
+                cout << "Enter Employee Name: ";
+                getline(cin, Employee_name);
+                cout << "Enter Employee Position: ";
+                getline(cin, employee_position);
+                cout << "Enter Employee Salary:";
+                cin>> employee_salary;
+                Employee::set_person_id(Employee_id);
+                Employee::set_person_name(Employee_name);
+                Employee::set_employee_position(employee_position);
+                Employee::set_employee_salary(employee_salary);
+            }
+            fout << Employee::tostring() << endl;
+        }
+        fin.close();
+        fout.close();
+        remove("Employee.txt");
+        rename("temp.txt", "Employee.txt");
+        if (!found) {
+            cout << "Employee with ID " << id << " not found." << endl;
+        }
+        else {
+            cout << "Employee updated successfully." << endl;
+        }
+    }
+    else {
+        cerr << "Error: Unable to open file for reading or writing." << endl;
+    }
+}
+void Employee::search() {
+    int id;
+    cout << "Enter the ID of the Employee to search: ";
+    cin >> id;
+    ifstream fin("Employee.txt");
+    if (fin.is_open()) {
+        bool found = false;
+        int option, Employee_id;
+        string Employee_name;
+        while (fin >> Employee_id >> Employee_name >> employee_position >> employee_salary) {
+            if (Employee_id == id) {
+                found = true;
+                cout << "\nEmployee ID: " << Employee_id;
+                cout << "\nEmployee Name: " << Employee_name;
+                cout << "\nEmployee Position: " << employee_position;
+                cout << "\nEmployee Salary: " << employee_salary;
+            }
+            fin.close();
+            if (!found) {
+                cout << "Employee with ID " << id << " not found." << endl;
+            }
+        }
+    }
+    else {
+        cerr << "Error: Unable to open file for reading." << endl;
+    }
+}
+void Employee::del() {
+    int id;
+    cout << "Enter the ID of the Employee to delete: ";
+    cin >> id;
+    ifstream fin("Employee.txt");
+    ofstream fout("temp.txt");
+    if (fin.is_open() && fout.is_open()) {
+        bool found = false;
+        int option, Employee_id;
+        string Employee_name;
+        while (fin >> Employee_id >> Employee_name >> employee_position >> employee_salary) {
+            if (Employee_id == id) {
+                found = true;
+            }
+            else {
+                fout << Employee_id << "\t" << Employee_name << "\t" << employee_position << "\t" << employee_salary << endl;
+            }
+        }
+        fin.close();
+        fout.close();
+        remove("Employee.txt");
+        rename("temp.txt", "Employee.txt");
+        if (!found) {
+            cout << "Employee with ID " << id << " not found." << endl;
+        }
+        else {
+            cout << "Employee with ID " << id << " deleted successfully." << endl;
+        }
+    }
+    else {
+        cerr << "Error: Unable to open file for reading or writing." << endl;
+    }
+}
+void Employee::menu(Employee& obj) {
+    int choice;
+    cout << "\nMenu 1- add 2- view 3- update 4- search 5- delete";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+    {
+        cin >> obj;
+        break;
+    }
+    case 2:
+    {
+        obj.view_from_file();
+        break;
+    }
+    case 3:
+    {
+        obj.update();
+        break;
+    }
+    case 4:
+    {
+        obj.search();
+        break;
+    }
+    case 5:
+    {
+        obj.del();
+        break;
+    }
+    default:
+    {
+        cout << "\nInvalid Choice";
+        break;
+    }
+    }
 }
 
 // Stream operators
 ostream& operator<<(ostream& cout, const Employee& obj) {
-    cout << "Employee Position: " << obj.employee_position << ", Salary: " << obj.employee_salary;
+    cout << "Employee ID: " << obj.get_person_id();
+    cout << "Employee Name: " << obj.get_person_name();
+    cout << "Employee Position: " << obj.employee_position;
+    cout << "Employee Salary: " << obj.employee_salary;
     return cout;
 }
 istream& operator>>(istream& cin, Employee& obj) {
-    cout << "Enter Employee Position: ";
+    int id;string name;
+    cout << "Employee ID: ";
+    cin>>id;
+    obj.set_person_id(id);
+    cin.ignore();
+    cout << "Employee Name: ";
+    getline(cin, name);
+    obj.set_person_name(name);
+    cout << "Employee Position: ";
     getline(cin, obj.employee_position);
-
-    cout << "Enter Employee Salary: ";
+    cout << "Employee Salary: ";
     cin >> obj.employee_salary;
+
+    obj.set_person_id(id);
+    obj.set_person_name(name);
+    obj.set_employee_position(obj.employee_position);
+    obj.set_employee_salary(obj.employee_salary);
+    
+    obj.store_to_file();
 
     return cin;
 }
